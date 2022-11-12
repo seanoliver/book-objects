de0c585d2af748d105a0e91e61425b1dc84fba3f
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2022-11-11 21:03:19 -0800

    update using class syntax

--- scripts.js
+++ scripts.js
@@ -7,15 +7,18 @@
 
 // theHobbit.info() // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 
-function Book (title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.info = function () {
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    info() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`
     }
 }
+
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
 
